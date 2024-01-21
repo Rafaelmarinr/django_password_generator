5a1ba6503270e53ed06d11ed5a31a1b398df7a8f
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-01-21 16:22:31 -0400

    Generate password

--- django_password_generator/urls.py
+++ django_password_generator/urls.py
@@ -21,5 +21,6 @@ from generator import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home)
+    path('', views.home),
+    path('password/', views.password),
 ]
